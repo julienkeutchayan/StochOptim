b6686d652ebd8a73dc995bfe985b7da92ac0fcc7
Julien_K <j.keutchayan@gmail.com>
2020-11-22 16:13:53 -0500

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stochoptim",
-    version="0.0.8",
+    version="0.0.10",
     author="Julien Keutchayan",
     author_email="j.keutchayan@gmail.com",
     description=("StochOptim is a Stochastic Optimization package with scenario generation tools "
